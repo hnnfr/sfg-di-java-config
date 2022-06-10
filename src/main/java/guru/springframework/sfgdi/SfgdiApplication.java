cd0d220f1a60d2c652a20a5cab6a21efb094c6c7
hnnfr <59726038+hnnfr@users.noreply.github.com>
2022-06-10 07:51:03 +0200

    Create Spring Boot project

---
+++ src/main/java/guru/springframework/sfgdi/SfgdiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgdiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgdi/SfgdiApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgdiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true, "Load the context");
+    }
+
+}
